9f10fdbf725f74797b35ae939bbdb126130ace2d
krabhi1 <abhishek33gl@gmail.com>
2024-11-14 10:34:54 +0530

    added worker

---
+++ api/index.ts
@@ -0,0 +1,20 @@
+// Note: You would need to compile your TS into JS and output it as a `_worker.js` file. We do not read `_worker.ts`
+import { hello } from './utils'
+interface Env {
+    ASSETS: Fetcher;
+}
+
+export default {
+    async fetch(request, env): Promise<Response> {
+        const url = new URL(request.url);
+        if (url.pathname.startsWith('/api/')) {
+            // TODO: Add your custom /api/* logic here.
+            return new Response(hello());
+        }
+        // Otherwise, serve the static assets.
+        // Without this, the Worker will error and no assets will be served.
+        return env.ASSETS.fetch(request);
+    },
+} satisfies ExportedHandler<Env>;
+
+
---
+++ api/utils.ts
@@ -0,0 +1,3 @@
+export function hello() {
+    return "this is good"
+}
