415ed1c3dbcb1a307f7a064c68650e384da20b7c
krabhi1 <abhishek33gl@gmail.com>
2024-12-04 14:55:09 +0530

    rename and fix uploadData, added changeCount

--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -10,17 +10,16 @@ export type Status = "new" | "deleted" | "updated" | "normal";
 export function getCollectionStatus(c: Collection): Status {
   let status: Status = "normal";
   if (c.isDeleted) status = "deleted";
-  else if (c._originalName == undefined) status = "new";
-  else if (c._originalName != c.name) status = "updated";
+  else if (c._name == undefined) status = "new";
+  else if (c._name != c.name) status = "updated";
   return status;
 }
 
 export function getItemStatus(c: CItem): Status {
   let status: Status = "normal";
   if (c.isDeleted) status = "deleted";
-  else if (c._originalkey == undefined) status = "new";
-  else if (c._originalkey != c.key || c._originalvalue != c.value)
-    status = "updated";
+  else if (c._key == undefined) status = "new";
+  else if (c._key != c.key || c._value != c.value) status = "updated";
   return status;
 }
 
